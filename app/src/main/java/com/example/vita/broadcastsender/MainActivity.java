963967e5cd7f518873866309fe60791d9a186e9d
vita888 <13793899@qq.com>
2017-07-04 10:50:28 +0800

    broadcast 发送器

---
+++ app/src/main/java/com/example/vita/broadcastsender/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.vita.broadcastsender;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendBroadcast(new Intent("com.example.vita.broadcastnotification"));
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+}
